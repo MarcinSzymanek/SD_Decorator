96894d0ee021771252dc5329e6a451f5ca9c065b
Marcin Szymanek <202009418@post.au.dk>
2022-10-31 13:23:44 +0100

    Fixed unused UnityEngine namespaces Added example attribute initialization + display code.

--- SD_DecoratorApp/SD_DecoratorApp/Program.cs
+++ SD_DecoratorApp/SD_DecoratorApp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Character;
+using Character.Decorators;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+MonsterBaseAttributes atr = new MonsterBaseAttributes();
+DecoratorBig dec = new DecoratorBig(atr);
+Console.WriteLine("Monster1\nDamage: " + dec.Damage + "\nHp: " +  dec.Hp + "\nSize: " + dec.ModelScale);
