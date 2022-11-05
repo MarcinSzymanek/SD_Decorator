605b26ae3e4ea249e50e11a4876f6503dde088ba
Marius Gjøderum-Svenningsen <ussi0506@gmail.com>
2022-11-05 19:26:29 +0100

    Intial monster generator commit

--- SD_DecoratorApp/SD_DecoratorApp/MonsterGenerator/MonsterGenerator.cs
+++ SD_DecoratorApp/SD_DecoratorApp/MonsterGenerator/MonsterGenerator.cs
@@ -1,13 +1,29 @@
 ﻿using System;
 using SD_DecoratorApp.Monsters;
+using SD_DecoratorApp.Attributes;
+//using Character;
 
-public class MonsterGenerator
+namespace SD_DecoratorApp.MonsterGenerator
 {
-	public MonsterGenerator()
-	{
-        Monster SpawnMonster()
-        {
-            return new Monster();
-        }
-	}
+    public class MonsterGenerator
+    {
+        Attribute attribute;
+	    public MonsterGenerator()
+	    {
+            Monster SpawnMonster()
+            {
+                return new Monster();
+            }
+            Monster SpawnSpecialMonster()
+            {
+                // Add decorator
+                return new Monster();
+            }
+            Monster SpawnMonster(Attribute attr)
+            {
+                return new Monster(attr);
+            }
+	    }
+    }
+
 }
